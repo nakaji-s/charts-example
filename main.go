3b738a3888c6a070a37589d8a10b147bbba66d8e
nakaji-s <nakajima.sota.sub@gmail.com>
2017-08-18 23:07:55 +0900

    Add to create random data for animate charts

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 
+	"math/rand"
+	"time"
+
 	"github.com/labstack/echo"
 )
 
@@ -10,12 +13,25 @@ type DataSet struct {
 	Data []int
 }
 
+func (d *DataSet) Add(num int) {
+	d.Data = append(d.Data[1:], num)
+}
+
 func main() {
 	e := echo.New()
 
 	e.File("/", "charts-example/index.html")
 
-	dataSet := DataSet{Data: []int{40, 39, 10, 40, 39, 80}}
+	dataSet := DataSet{Data: []int{}}
+	for i := 0; i < 100; i++ {
+		dataSet.Data = append(dataSet.Data, rand.Intn(100))
+	}
+	go func() {
+		for {
+			dataSet.Add(rand.Intn(100))
+			time.Sleep(1 * time.Second)
+		}
+	}()
 
 	e.GET("/data", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, dataSet)
